779bbb346f0b51dc16aacc804b4b440b1ad6216b
jcanales <jcanales@webdoxclm.com>
2025-03-23 13:54:31 -0300

    feat: add form to search jobs

--- lib/jobs_app_web/live/jobs_live.ex
+++ lib/jobs_app_web/live/jobs_live.ex
@@ -2,25 +2,20 @@ defmodule JobsAppWeb.JobsLive do
   use JobsAppWeb, :live_view
 
   import JobsAppWeb.JobsLive.Components,
-    only: [job_detail_modal: 1, job_row: 1]
+    only: [job_detail_modal: 1, job_row: 1, search_job_form: 1]
 
   alias JobsApp.Jobs
   alias Phoenix.PubSub
   @pub_sub_topic "new_jobs"
 
   @impl true
+  def mount(%{"search_text" => _search_text} = search_params, _session, socket) do
+    {:ok, filter_jobs(socket, search_params)}
+  end
+
+  @impl true
   def mount(_params, _session, socket) do
-    PubSub.subscribe(JobsApp.PubSub, @pub_sub_topic)
-
-    page = 1
-    Task.async(fn -> paginate_jobs(page) end)
-
-    socket =
-      socket
-      |> assign(refresh_jobs: false, loading_jobs: true, page: page, end_of_timeline?: false)
-      |> stream(:jobs, [])
-
-    {:ok, socket}
+    {:ok, filter_jobs(socket)}
   end
 
   @impl true
@@ -65,7 +60,7 @@ defmodule JobsAppWeb.JobsLive do
   @impl true
   def handle_event("next-page", _params, socket) do
     page = socket.assigns.page + 1
-    Task.async(fn -> paginate_jobs(page) end)
+    Task.async(fn -> paginate_jobs(page, socket.assigns.search_params) end)
     {:noreply, assign(socket, loading_jobs: true, page: page)}
   end
 
@@ -101,6 +96,8 @@ defmodule JobsAppWeb.JobsLive do
         </.link>
       </div>
 
+      <.search_job_form form={@search_form} />
+
       <div id="jobs" phx-update="stream" phx-viewport-bottom={!@end_of_timeline? && "next-page"}>
         <.job_row :for={{dom_id, job} <- @streams.jobs} id={dom_id} job={job} />
       </div>
@@ -117,6 +114,15 @@ defmodule JobsAppWeb.JobsLive do
     """
   end
 
+  defp apply_action(:index, %{"search_text" => _search_text} = search_params, socket) do
+    page = 1
+    Task.async(fn -> paginate_jobs(page, search_params) end)
+
+    socket
+    |> assign(loading_jobs: true, page: page, search_params: search_params)
+    |> stream(:jobs, [], reset: true)
+  end
+
   defp apply_action(:index, _params, socket) do
     socket
   end
@@ -127,8 +133,26 @@ defmodule JobsAppWeb.JobsLive do
     assign(socket, job: job)
   end
 
-  defp paginate_jobs(page) do
-    :timer.sleep(3000)
-    Jobs.list_jobs(page)
+  defp paginate_jobs(page, search_params) do
+    Jobs.list_jobs(page, search_params)
+  end
+
+  defp filter_jobs(socket, search_params \\ %{}) do
+    PubSub.subscribe(JobsApp.PubSub, @pub_sub_topic)
+    search_form = to_form(search_params)
+
+    page = 1
+    Task.async(fn -> paginate_jobs(page, search_params) end)
+
+    socket
+    |> assign(
+      refresh_jobs: false,
+      loading_jobs: true,
+      page: page,
+      end_of_timeline?: false,
+      search_form: search_form,
+      search_params: search_params
+    )
+    |> stream(:jobs, [])
   end
 end
